d6e77608dd7c691dde50ec365352ae4e77df7a86
aden <adenali362@gmail.com>
2025-12-31 04:24:13 -0600

    use https and remove ports

--- src/api/axios.js
+++ src/api/axios.js
@@ -9,8 +9,8 @@ const isLocalhost = url.hostname === 'localhost' || url.hostname === '127.0.0.1'
 // For localhost, replace the port with 8050.
 // For deployed (non-local) environments, just use the same origin.
 const base = isLocalhost
-  ? `http://dev.adenali.com:8080/api/authservice`
-  : `http://dev.adenali.com:8080/api/authservice`;
+  ? `http://localhost:8080/api/authservice`
+  : `https://dev.adenali.com/api/authservice`;
 
 const axiosInstance = axios.create({
   baseURL: base,
