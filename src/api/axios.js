dfd785d4f493cdcf8d2482cd319a4af25be560d5
aden <adenali362@gmail.com>
2025-12-30 18:30:11 -0600

    use dynamic api

--- src/api/axios.js
+++ src/api/axios.js
@@ -10,7 +10,7 @@ const isLocalhost = url.hostname === 'localhost' || url.hostname === '127.0.0.1'
 // For deployed (non-local) environments, just use the same origin.
 const base = isLocalhost
   ? `http://localhost:8080/api/authservice`
-  : `http://localhost:8080/api/authservice`;
+  : `${origin}:8080/api/authservice`;
 
 const axiosInstance = axios.create({
   baseURL: base,
